f479ab71fbf5130b11284533ea0380b28f074181
Jayara-PC\Jayara <jajamedeiros43@gmail.com>
2016-10-20 14:55:28 -0300

    Início da elaboração da página home

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+function login(){
+
+		console.log("Validadon login");
+		/*var conf=0;
+		var usr = document.getElementById('inputUsuario').value;
+		usr = usr.toLowerCase();
+		var psw = document.getElementById("inputSenha").value;
+		psw=psw.toLowerCase();
+		if (usr=="admin" && psw=="admin") {
+			window.location="../HTML/home.html";
+			conf=1;
+		}
+		if (conf==0) { alert("Dados incorretos, tente novamente"); }*/
+}	
